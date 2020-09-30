997eda99348ba3bfa116118438ab527624f6563d
David Zhang <david@primetrust.com>
2020-09-30 15:23:14 -0700

    remove unused options, allow matching branch name on capture group

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ async function run() {
       branchRegex: core.getInput('branch-regex', {required: true}),
       lowercaseBranch: (core.getInput('lowercase-branch').toLowerCase() === 'true'),
       titleTemplate: core.getInput('title-template', {required: true}),
-      replaceTitle: (core.getInput('replace-title').toLowerCase() === 'true'),
-      titlePrefixSpace: (core.getInput('title-prefix-space').toLowerCase() === 'true'),
-      uppercaseTitle: (core.getInput('uppercase-title').toLowerCase() === 'true'),
       bodyTemplate: core.getInput('body-template', {required: true}),
-      replaceBody: (core.getInput('replace-body').toLowerCase() === 'true'),
       bodyPrefixNewlineCount: parseInt(core.getInput('body-prefix-newline-count', {required: true})),
       uppercaseBody: (core.getInput('uppercase-body').toLowerCase() === 'true'),
     }
@@ -25,12 +21,12 @@ async function run() {
 
     const matches = branch.match(new RegExp(inputs.branchRegex));
     if (!matches) {
-      core.setFailed('Branch name does not match given regex');
+      core.info('Branch name does not match given regex');
       return;
     }
 
-    const match = (upperCase) => upperCase ? matches[0].toUpperCase() : matches[0];
-    core.info(`Matched branch text: ${match(false)}`);
+    const match = matches[matches.length - 1]; // last capturing group
+    core.info(`Matched branch text: "${matches[0]}" on group: "${match}"`);
 
     const request = {
       owner: github.context.repo.owner,
@@ -39,34 +35,26 @@ async function run() {
     }
 
     const title = github.context.payload.pull_request.title || '';
-    const processedTitle = inputs.titleTemplate.replace(tokenRegex, match(inputs.uppercaseTitle));
+    const processedTitle = inputs.titleTemplate.replace(tokenRegex, match);
     core.debug(`processedTitle: ${processedTitle}`);
 
-    const updateTitle = inputs.replaceTitle
-      ? title.toLowerCase() !== processedTitle.toLowerCase()
-      : !title.toLowerCase().startsWith(processedTitle.toLowerCase());
+    const updateTitle = !title.toLowerCase().startsWith(processedTitle.toLowerCase());
 
     if (updateTitle) {
-      request.title = inputs.replaceTitle
-      ? processedTitle
-      : processedTitle.concat(inputs.titlePrefixSpace ? ' ': '', title);
+      request.title = processedTitle.concat(' ', title);
       core.debug(`new title: ${request.title}`);
     } else {
       core.warning('PR title is up to date already - no updates made');
     }
 
     const body = github.context.payload.pull_request.body || '';
-    const processedBody = inputs.bodyTemplate.replace(tokenRegex, match(inputs.uppercaseBody));
+    const processedBody = inputs.bodyTemplate.replace(tokenRegex, match);
     core.debug(`processedBody: ${processedBody}`);
 
-    const updateBody = inputs.replaceBody
-      ? body.toLowerCase() !== processedBody.toLowerCase()
-      : !body.toLowerCase().startsWith(processedBody.toLowerCase());
+    const updateBody = !body.toLowerCase().startsWith(processedBody.toLowerCase());
 
     if (updateBody) {
-      request.body = inputs.replaceBody
-      ? processedBody
-      : processedBody.concat('\n'.repeat(inputs.bodyPrefixNewlineCount), body);
+      request.body = processedBody.concat('\n'.repeat(inputs.bodyPrefixNewlineCount), body);
       core.debug(`new body: ${request.body}`);
     } else {
       core.warning('PR body is up to date already - no updates made');
